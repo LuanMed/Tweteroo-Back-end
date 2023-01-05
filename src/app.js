f37f07776b360d115c432ceaf0a35bf1ce849e06
LuanMed <luanmede@gmail.com>
2023-01-05 14:42:13 -0300

    Implemented feature: post /sign-up

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+
+let users = [];
+let tweets = {a:{b:"b"},c:"c",d:"d"}
 
 const app = express();
+app.use( bodyParser.json() );
 
 app.use(cors());
 
 app.post("/sign-up", (req, res) => {
-    res.send('Hello Cold World');
+    console.log(req.body);
+    users.push(req.body);
+    console.log(users);
+    res.send('OK');
 });
 
 app.post("/tweets", (req, res) => {
@@ -14,7 +22,9 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (req, res) => {
-    res.send('Hello Cold World');
+    res.send(tweets);
 });
 
-app.listen(5000);
+app.listen(5000, () => {
+    console.log("Servidor rodando")
+});
